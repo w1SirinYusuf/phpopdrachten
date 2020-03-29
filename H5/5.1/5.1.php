c10bc5a1c0d9f4a27da0f79bbf517bd9d37dd375
Yusuf <yusuf.sirin@edu-kw1c.nl>
2020-03-29 23:10:59 +0200

    opdracht 4.2

---
+++ H5/5.1/5.1.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8" />
+    <title>Opdrachten PHP periode 3</title>
+</head>
+<body>
+<header><h1>ICT Academie AO</h1></header>
+<div id="wrapper">
+
+<img src="KEES.png" alt="keeskroket" width="25%" height="25%">
+<form  action="form_data.php" method="get">
+
+    <table>
+        <tr>
+            <td>
+                <label>Naam: </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="text" name="naam">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>E-mail </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="text" name="email">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>Onderwerp</label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="text" name="onderwerp">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>Bericht </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="text" name="bericht" >
+            </td>
+        </tr>
+
+    </table>
+    <input type="submit">
+    <br><br>
+</form>
+
---
+++ H5/5.1/form_data.php
@@ -0,0 +1,15 @@
+
+<table>
+    <?php
+    echo "<table class='eenvoudig'>";
+    echo "<tr><td>naam</td><td>".$_GET["naam"]."</td></tr>";
+    echo "<tr><td>email</td><td>".$_GET["email"]."</td></tr>";
+    echo "<tr><td>Is het
+Onderwerp</td><td>".$_GET["onderwerp"]."</td></tr>";
+    echo "<tr><td>bericht</td><td>".$_GET["bericht"]."</td></tr>";
+    echo "</table>";
+    ?>
+
+
+
+</table>
